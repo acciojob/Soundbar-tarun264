35a3fce0c11e6ada4fb812c72193ec0c38858075
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-09-29 01:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+
+function applause(){
+	const audio=document.createElement("audio");
+	audio src="./sounds/applause.mp4";
+
+	audio.play();	
+}
+
+function boo(){
+	const audio=document.createElement("audio");
+	audio src="./sounds/boo.mp4";
+	audio.play();	
+}
+
+function gasp(){
+	const audio=document.createElement("audio");
+	audio src="./sounds/gasp.mp4";
+	audio.play();
+}
+
+function tada(){
+	const audio=document.createElement("audio");
+	audio src="./sounds/tada.mp4";
+	audio.play();
+}
+function victory() {
+	const audio=document.createElement("audio");
+	audio src="./sounds/victory.mp4";
+	audio.play();
+}
+function wrong() {
+	const audio=document.createElement("audio");
+	audio src="./sounds/wrong.mp4";
+	audio.play();	
+}
+
+function stop() {
+const audio=document.createElement("audio");
+	audio src="./sounds/stop.mp4";
+	audio.play();	
+}
